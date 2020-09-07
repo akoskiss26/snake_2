e9b9456c0924949fbc3e43ad41e260c8c4368446
Ákos Kiss <akos.kiss26@gmail.com>
2020-09-07 18:06:07 +0200

    #3: Az időszámlálás és annak kijelzése

--- Snake_2/Model/Arena.cs
+++ Snake_2/Model/Arena.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace Snake_2.Model
 {
     class Arena
     {
         private MainWindow View;
+        private DispatcherTimer pendulumForPlayTime;
+        private int playTime = 0;
 
         public Arena(MainWindow view)
         {
@@ -18,7 +21,16 @@ namespace Snake_2.Model
             // A játék kezdetén megjelenítjük a játékszabályokat
             View.GameplayTextBlock.Visibility = System.Windows.Visibility.Visible;
 
+            //taktjeladó
+            pendulumForPlayTime = new DispatcherTimer(TimeSpan.FromSeconds(1),DispatcherPriority.Normal, CountPlayTime, Dispatcher.CurrentDispatcher);
+            pendulumForPlayTime.Start();
 
+
+        }
+
+        private void CountPlayTime(object sender, EventArgs e)
+        {
+            playTime = playTime + 1;
         }
 
         internal void KeyDown(KeyEventArgs e)
@@ -35,11 +47,16 @@ namespace Snake_2.Model
                     View.GameplayTextBlock.Visibility = System.Windows.Visibility.Hidden;
                     View.NumberOfMeals.Visibility = System.Windows.Visibility.Visible;
                     Console.WriteLine(e.Key);
+                    Console.WriteLine(playTime);
+                    View.NumberOfMeals.Text = Convert.ToString(playTime);
                     break;
                 
             }
 
 
         }
+
+
+
     }
 }
