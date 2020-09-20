d2ee340c992f68a1e903395c5178ebf35235b28a
Ákos Kiss <akos.kiss@amcnetworks.com>
2020-09-21 00:21:54 +0200

    3# Akígyófej megjelenítése

--- Snake_2/Model/Arena.cs
+++ Snake_2/Model/Arena.cs
@@ -19,7 +19,7 @@ namespace Snake_2.Model
             this.View = view;
 
             // A játék kezdetén megjelenítjük a játékszabályokat
-            View.GameplayTextBlock.Visibility = System.Windows.Visibility.Visible;
+            View.GamePlayBorder.Visibility = System.Windows.Visibility.Visible;
 
             //taktjeladó
             pendulumForPlayTime = new DispatcherTimer(TimeSpan.FromSeconds(1),DispatcherPriority.Normal, CountPlayTime, Dispatcher.CurrentDispatcher);
@@ -44,12 +44,23 @@ namespace Snake_2.Model
                 case Key.Right:
                 case Key.Down:
                     //elindul a játék, eltüntetjük a játékszabályokat, mutatjuk az eredményt
-                    View.GameplayTextBlock.Visibility = System.Windows.Visibility.Hidden;
+                    View.GamePlayBorder.Visibility = System.Windows.Visibility.Hidden;
                     View.NumberOfMeals.Visibility = System.Windows.Visibility.Visible;
                     View.ArenaGrid.Visibility = System.Windows.Visibility.Visible;
                     Console.WriteLine(e.Key);
                     Console.WriteLine(playTime);
                     View.NumberOfMeals.Text = Convert.ToString(playTime);
+
+                    //kígyófej megjelenítése a Children gyűjteménnyel
+                    var cell = View.ArenaGrid.Children[10 * 20 + 10];
+
+                    //mivel egy általános ement típust   kaptunk vissza, azt konvertálni kell:
+          
+                    var image = (FontAwesome.WPF.ImageAwesome)cell;
+
+                    // már el tudim érni az icon tulajdonságait
+                    image.Icon = FontAwesome.WPF.FontAwesomeIcon.Circle;
+
                     break;
                 
             }
